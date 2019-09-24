f1bfc8be4840524bde97db0f02b264a08069de72
northerchild <osarus-13@hotmail.com>
2019-09-24 18:47:27 -0500

    Se crea el contex Api

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React,{Fragment} from 'react';
+import React from 'react';
 import Header from './components/Header'
+import CategoriasProvider from './context/CategoriaContex';
 
 function App() {
   return (
-    <Fragment>
+    <CategoriasProvider>
     <Header />
-    </Fragment>
+    </CategoriasProvider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Header = () => (
     <header className="uk-margin" uk-margin="true">
-        <h1 className="uk-text-center">Eventos En React Con EventBrite API</h1>
+        <h1 className="uk-text-center">Eventos En React Con EventBrite API </h1>
     </header>
 );
 
