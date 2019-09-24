b02e45d31f342131a7de197f85e6af0d4bd2e571
northerchild <osarus-13@hotmail.com>
2019-09-24 14:56:10 -0500

    Se crea el header

---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => (
+    <header className="uk-margin" uk-margin="true">
+        <h1 className="uk-text-center">Eventos En React Con EventBrite API</h1>
+    </header>
+);
+
+export default Header
