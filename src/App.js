2dcaacf8bcfcb320293a808f37d094c94f0955e8
northerchild <osarus-13@hotmail.com>
2019-09-24 19:40:33 -0500

    Se agrega el formulario

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import Header from './components/Header'
 import CategoriasProvider from './context/CategoriaContex';
+import Formulario from './components/Formulario'
 
 function App() {
   return (
     <CategoriasProvider>
     <Header />
+    <div className="uk-container">
+      <Formulario />
+    </div>
+    
     </CategoriasProvider>
   );
 }
---
+++ src/components/Formulario.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+class Formulario extends Component {
+    state = {
+        nombre: '',
+        categirua: ''
+    }
+    render() {
+        return (
+            <form  >
+                            <fieldset className="uk-fieldset uk-margin">
+                                <legend className="uk-legend uk-text-center">
+                                    Busca tu evento por Nombre o Categoría
+                                </legend>
+                            </fieldset>
+
+                            <div className="uk-column-1-3@m uk-margin">
+                                <div className="uk-margin" uk-margin="true">
+                                        <input
+                                            name="nombre"
+                                            className="uk-input"
+                                            type="text"
+                                            placeholder="Nombre de Evento o Ciudad"
+                                            onChange={this.obtenerDatosEvento}
+                                        />
+                                </div>
+
+                                <div className="uk-margin" uk-margin="true">
+                                    <select
+                                        className="uk-select"
+                                        name="categoria"
+                                        onChange={this.obtenerDatosEvento}
+                                    >
+                                        <option value="">--Selecciona Categoría --</option>
+                                    </select>
+                                </div>
+
+                                <div>
+                                    <input type="submit" className="uk-button uk-button-danger" value="Busca Eventos" />
+                                </div>
+                            </div>
+                        </form>
+        );
+    }
+}
+
+export default Formulario;
